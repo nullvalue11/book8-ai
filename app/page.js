06dc5516169dceae75aa3323285fda47e64048b3
nullvalue11 <waism@live.ca>
2025-10-23 05:59:53 +0000

    auto-commit for 2bfeee5a-5e5d-4e6e-a255-b322f387bb5d

--- app/page.js
+++ app/page.js
@@ -1,7 +1,3 @@
-// Server wrapper page to avoid prerender issues and keep SSG/ISR controls
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
 import HomeClient from './HomeClient'
 
 export default function Page() {
