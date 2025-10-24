18fabbccff8b432e16175e783e0e350e1d369449
nullvalue11 <waism@live.ca>
2025-10-24 03:06:15 +0000

    auto-commit for 9a24ccf8-e4dd-40b1-ade4-305b29cd642d

---
+++ components/ui/card.tsx
@@ -0,0 +1,16 @@
+"use client";
+import * as React from "react";
+
+export function Card({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`rounded-lg border bg-card text-card-foreground ${className}`} {...props} />
+}
+export function CardHeader({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`p-6 pb-0 ${className}`} {...props} />
+}
+export function CardTitle({ className = "", ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
+  return <h3 className={`text-lg font-semibold leading-none tracking-tight ${className}`} {...props} />
+}
+export function CardContent({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`p-6 ${className}`} {...props} />
+}
+export default Card;
