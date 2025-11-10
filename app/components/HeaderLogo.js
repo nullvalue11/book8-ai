6e03ce3ddba468f15eea6b50bf8de382f8a87357
nullvalue11 <waism@live.ca>
2025-11-10 04:23:38 +0000

    auto-commit for 405f8237-dde0-41a5-a80e-a97896a15447

--- app/components/HeaderLogo.js
+++ app/components/HeaderLogo.js
@@ -8,8 +8,8 @@ export default function HeaderLogo() {
         src="/brand/book8_ai_logo.svg"
         alt="Book8 AI"
         width={152}
-        height={28}
-        className="opacity-90 hover:opacity-100 transition-opacity duration-150 ease-out"
+        height={32}
+        className="h-7 md:h-8 w-auto opacity-90 hover:opacity-100 transition-opacity duration-150 ease-out"
         priority
       />
     </Link>
