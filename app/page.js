d436380e08d8ba2dc0ed8cd1ec6a5c243547613d
nullvalue11 <waism@live.ca>
2025-11-10 03:13:41 +0000

    auto-commit for 69fe1705-2a82-467c-bd75-eedaf89dd90a

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ import { Label } from "./components/ui/label";
 import { Textarea } from "./components/ui/textarea";
 import AnalyticsDashboard from "./components/AnalyticsDashboard";
 import Header from "./components/Header";
+import HeaderLogo from "./components/HeaderLogo";
 import HomeHero from "./(home)/HomeHero";
 import { useTheme } from "next-themes";
 import { QrCode, Share2, Settings, ExternalLink, Check, Moon, Sun } from "lucide-react";
