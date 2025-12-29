c05f0333bdfae7dfb1d076bc950b4a866f7927f9
nullvalue11 <waism@live.ca>
2025-12-29 17:33:17 +0000

    auto-commit for f8fee6eb-7c76-4f23-9ea2-79ef78c3d8e6

--- app/components/IPhoneMockup.js
+++ app/components/IPhoneMockup.js
@@ -4,19 +4,17 @@ import React from "react";
 
 export default function IPhoneMockup() {
   return (
-    <div className="relative w-full max-w-[550px] lg:max-w-[650px] mx-auto">
+    <div className="relative w-full max-w-[600px] lg:max-w-[700px] mx-auto overflow-hidden">
       {/* Hand holding iPhone image - cropped to show ONLY phone and hand */}
-      <div 
-        className="relative overflow-hidden"
-      >
+      <div className="relative">
         <img
           src="https://customer-assets.emergentagent.com/job_ops-api/artifacts/kexqk3mb_image.png"
           alt="Book8-AI mobile app"
           className="w-full h-auto"
           style={{
-            clipPath: 'inset(10% 0 42% 0)',
-            transform: 'scale(1.2)',
-            transformOrigin: 'center top',
+            clipPath: 'inset(8% 0 45% 0)',
+            transform: 'scale(1.25)',
+            transformOrigin: 'center 30%',
           }}
         />
       </div>
